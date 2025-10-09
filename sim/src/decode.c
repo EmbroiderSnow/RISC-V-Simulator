4a1f842c442a8e57f44f251a99d4cd6618225166
EmbroiderSnow <1497411439@qq.com>
2025-10-09 09:44:36 +0800

    feat(decode): type B

--- sim/src/decode.c
+++ sim/src/decode.c
@@ -26,6 +26,10 @@ enum {
                                 BITS(i, 20, 20) << 11 | \
                                 BITS(i, 30, 21) << 1, 21);} while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31) << 12 | \
+                                BITS(i, 7, 7) << 11 | \
+                                BITS(i, 30, 25) << 5 | \
+                                BITS(i, 11, 8) << 1, 13);} while(0)
 
 static void decode_operand(Decode *s, int *rd, uint64_t *src1, uint64_t *src2, uint64_t *imm, int type){
     uint32_t i = s->inst;
@@ -37,6 +41,7 @@ static void decode_operand(Decode *s, int *rd, uint64_t *src1, uint64_t *src2, u
         case TYPE_U:                   immU(); break;
         case TYPE_J:                   immJ(); break;
         case TYPE_S: src1R(); src2R(); immS(); break;
+        case TYPE_B: src1R(); src2R(); immB(); break;
     }
 }
 void decode_exec(Decode *s){
@@ -50,14 +55,30 @@ void decode_exec(Decode *s){
   __VA_ARGS__ ; \
 }
     INSTPAT_START();
-    //RV64IM
+    //RV64I
     INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui, U, R(rd) = imm);
     INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
     INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->pc + 4, s->dnpc = s->pc + imm);
     INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(rd) = s->pc + 4, s->dnpc = (src1 + imm) & ~1);
+    // BEQ, BNE, BLT, BGE, BLTU, BGEU
+    // LB, LH, LW, LBU, LHU, LWU, LD
+    // SB, SH, SW
     INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + imm, 8, src2));
     INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
+    // SLTI, SLTIU, SLLI, SRLI, SRAI
+    // XORI, ORI, ANDI
+    // ADDIW
+    // SLLI, SRLI, SRAI, SLLIW, SRLIW, SRAIW
+    // ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
+    // ADDW, SUBW, SLLW, SRLW, SRAW
+    // FENCE, FENCE.I
+    // ECALL
     INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, HALT(s->pc, R(10))); // R(10) is $a0
+    // CSRRW, CSRRS, CSRRC, CSRRWI, CSRRSI, CSRRCI
+
+    // RV64M
+    // MUL, MULH, MULHSU, MULHU, DIV, DIVU, REM, REMU
+    // MULW, DIVW, DIVUW, REMW, REMUW
 
     //Invalid Opcode
     INSTPAT("??????? ????? ????? ??? ????? ????? ??", unk    , N, printf(ANSI_FMT("Unknown Inst!\n", ANSI_FG_RED)), HALT(s->pc, -1));
