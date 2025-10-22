e02b088dfb416494f269d7cf11fb1949d3b7f2cc
EmbroiderSnow <1497411439@qq.com>
2025-10-22 14:26:17 +0800

    refactor(main&makefile): support different processor model.

--- Makefile
+++ Makefile
@@ -1,44 +1,55 @@
+T ?= dummy
+MODEL ?= iss
+ARGS ?=
+
 SIM = sim/build/Simulator
 TARGET = test/build/$(T).bin
 
-all:
+.PHONY: all build build-sim build-test
+
+all: build
+build: build-sim build-test
+
+build-sim:
 	@echo "-------Build Simulator-------"
 	@$(MAKE) -C sim
+
+build-test:
 	@echo "-------Build Test-------"
 	@$(MAKE) -C test T=$(T)
+
+.PHONY: run iss mc debug itrace ftrace
+
+# Usage:
+# 	make run T=dummy MODEL=mc
+#	make run T=dummy MODEL=iss ARGS="--itrace"
+run: build
 	@echo "-------Start Simulation-------"
-	@$(SIM) $(T) --batch
+	@echo "  MODEL: $(MODEL)"
+	@echo "  IMAGE: $(TARGET)"
+	@echo "  ARGS:  $(ARGS)"
+	@echo "------------------------------"
+	@$(SIM) $(MODEL) $(T) $(ARGS)
+
+# run iss
+# Usage: make iss T=dummy
+iss: 
+	@$(MAKE) run MODEL=iss T=$(T) ARGS="--batch"
+
+mc:
+	@$(MAKE) run MODEL=mc T=$(T) ARGS=""
 
 debug: 
-	@echo "-------Build Simulator-------"
-	@$(MAKE) -C sim
-	@echo "-------Build Test-------"
-	@$(MAKE) -C test T=$(T)
-	@echo "-------Start Debugging-------"
-	@$(SIM) $(T) --debug
+	@$(MAKE) run MODEL=iss T=$(T) ARGS="--debug"
 
 itrace: 
-	@echo "-------Build Simulator-------"
-	@$(MAKE) -C sim
-	@echo "-------Build Test-------"
-	@$(MAKE) -C test T=$(T)
-	@echo "-------Start Debugging-------"
-	@$(SIM) $(T) --itrace
+	@$(MAKE) run MODEL=iss T=$(T) ARGS="--itrace"
 
 ftrace: 
-	@echo "-------Build Simulator-------"
-	@$(MAKE) -C sim
-	@echo "-------Build Test-------"
-	@$(MAKE) -C test T=$(T)
-	@echo "-------Start Debugging-------"
-	@$(SIM) $(T) --ftrace
+	@$(MAKE) run MODEL=iss T=$(T) ARGS="--ftrace"
 
-unit_test:
-	@echo "------- Building and Running Simulator Unit Tests -------"
-	@$(MAKE) -C sim unit_test
+.PHONY: clean 
 
 clean:
 	@$(MAKE) -C sim clean
 	@$(MAKE) -C test clean
-
-.PHONY: clean all unit_test
--- sim/src/main.c
+++ sim/src/main.c
@@ -10,12 +10,42 @@ int ftrace_enabled = 0;
 SymbolTable *sym_table = NULL;
 LLVMDisasmContextRef disasm_ctx;
 
-const char *help_string = "Usage: Simulator <img_file> [-d|-b]\n"
-                                  "Options:\n"
-                                  "  -d    run in debug mode\n"
-                                  "  -b    run in batch mode\n";
+const char *help_string = "Usage: Simulator <model> <img_file> [options]\n"
+                                  "Models:\n"
+                                  "  iss    Instruction Set Simulator\n"
+                                  "  mc     Multi-cycle Performance Simulator\n"
+                                  "  pl     Pipeline Performance Simulator\n";
+
+int run_iss_model(int argc, char *argv[]);
+int run_mc_model(int argc, char *argv[]);
+int run_pl_model(int argc, char *argv[]);
 
 int main(int argc, char *argv[]){
+    printf("argc: %d\n", argc);
+    if (argc < 3) {
+        printf("%s\n", help_string);
+    }
+
+    char *model = argv[1];
+
+    if (strcmp(model, "iss") == 0) {
+        run_iss_model(argc - 1, argv + 1);
+    }
+    else if (strcmp(model, "mc") == 0) {
+        run_mc_model(argc - 1, argv + 1);
+    }
+    else if (strcmp(model, "pl") == 0) {
+        run_pl_model(argc - 1, argv + 1);
+    }
+    else {
+        printf("Error: Unknown model '%s'.\n", model);
+        return -1;
+    }
+
+    return 0;
+}
+
+int run_iss_model(int argc, char *argv[]) {
     mem = (uint8_t *)malloc(MEM_SIZE);
     check_mem(mem);
     memset(mem, 0, MEM_SIZE);
@@ -57,4 +87,28 @@ int main(int argc, char *argv[]){
 error:
     if(mem) free(mem);
     return -1;
+}
+
+int run_mc_model(int argc, char *argv[]) {
+    mem = (uint8_t *)malloc(MEM_SIZE);
+    check_mem(mem);
+    memset(mem, 0, MEM_SIZE);
+    
+    char image_file[128] = "";
+    sprintf(image_file, "test/build/%s.bin", argv[1]);
+    load_image(image_file);
+
+    // TODO: multi_cycle_cpu_exec()
+
+    free(mem);
+    return 0;
+
+error:
+    if(mem) free(mem);
+    return -1;
+}
+
+int run_pl_model(int argc, char *argv[]) {
+    // TODO: pipeline model
+    return 0;
 }
